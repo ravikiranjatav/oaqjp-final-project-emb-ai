ba00d154f6bb38f0dd615f91eb90939bf5b58f9b
ravikiranjatav <ravikiranjatav@live.in>
2026-02-22 12:41:34 -0500

    add unit test

--- server.py
+++ server.py
@@ -9,9 +9,26 @@ def home():
 
 @app.route("/emotionDetector")
 def detect_emotion():
-    text = request.args.get("textToAnalyze")
-    result = emotion_detector(text)
-    return str(result)
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    result = emotion_detector(text_to_analyze)
+
+    # API Failure/Blank Input
+    if result is None or "dominant_emotion" not in result:
+        return "Invalid text! Please try again."
+
+    response_text = (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant emotion']}."
+    )
+
+
+    return response_text
 
 if __name__ == "__main__":
     app.run(debug=True)
